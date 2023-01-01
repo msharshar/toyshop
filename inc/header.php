fc3754ffc60b3b794608e52a66a8bd7f45e05807
Muhammed-Waleed <mw.muhammed.waleed@gmail.com>
2023-01-01 18:59:12 +0200

    Navbar design

--- inc/header.php
+++ inc/header.php
@@ -6,6 +6,7 @@
         <title>The Toy Shop</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
         <link rel="stylesheet" href="layout/css/style.css">
     </head>
     <body>
